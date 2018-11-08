ea576f77e1562fdf55a4d420ef1c4c95b13c051c
zhangyy <zhang10092009@hotamil.com>
2018-11-08 13:39:32 +0800

    test

---
+++ src/test/java/com/forteach/education/Test.java
@@ -0,0 +1,11 @@
+package com.forteach.education;
+
+/**
+ * @Auther: zhangyy
+ * @Email: zhang10092009@hotmail.com
+ * @Date: 18-11-8 13:38
+ * @Version: 1.0
+ * @Description:
+ */
+public class Test {
+}
