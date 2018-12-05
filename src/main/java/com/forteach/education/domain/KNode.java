6ed5399adade6e6272721e834cb2f52337550f35
ang00 <zhang10092009@hotmail.com>
2018-12-05 17:49:20 +0800

    add 12-5 18:00

--- src/main/java/com/forteach/education/domain/KNode.java
+++ src/main/java/com/forteach/education/domain/KNode.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
  * @Description:
  */
 @Entity
-@Table(name = "k_node")
+@Table(name = "k_node", indexes = {@Index(columnList = "k_node_id"), @Index(columnList = "course_id")})
 @Data
 @Builder
 public class KNode {
@@ -28,4 +28,19 @@ public class KNode {
     @ApiModelProperty(value = "知识点ID", name = "k_node_Id", dataType = "string")
     @Column(name = "k_node_Id", columnDefinition = "VARCHAR(32) COMMENT '知识点编号'")
     private String kNodeId;
+
+//    @Column(columnDefinition = )
+    private String nodeDesc;
+    @Column(name = "course_id", columnDefinition = "VARCHAR(32) COMMENT '科目课程ID'")
+    private String courseId;
+
+    @Column(name = "chapter_id", columnDefinition = "VARCHAR(32) COMMENT '章节ID'")
+    private String chapterId;
+
+    @Column(name = "data_id", columnDefinition = "VACHAR(32) COMMENT '资料ID'")
+    private String dataId;
+
+    @Column(name = "k_node_type", columnDefinition = "VARCHAR(32) COMMENT '知识点类型１ 单个知识点，２　多个知识点'")
+    private String kNodeType;
+
 }
